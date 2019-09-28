ccbe49403f1561e89d4d32fe03f56b742de2f713
elder-tome <r14.elderalves@gmail.com>
2019-09-28 16:34:06 -0300

    add project

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { YellowBox } from 'react-native'
+
+import Routes from './routes'
+
+YellowBox.ignoreWarnings([
+    'Unrecognized WebSocket'
+])
+
+export default function App() {
+
+    return(
+        <Routes />
+    )
+    
+}
